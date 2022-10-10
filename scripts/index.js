8d64e1ba96bb36ef5fe330931840fd8247369fc3
unknown <german280105@mail.ru>
2022-10-10 22:41:44 +0300

    Добавил готовую валидацию

--- scripts/index.js
+++ scripts/index.js
@@ -21,6 +21,7 @@ const popupCardImage = popupCard.querySelector('.popup-card__image');
 const popupCardCloseButton = popupCard.querySelector('.popup-card__close');
 const popupCardCaption = popupCard.querySelector('.popup-card__caption');
 
+
 for (let i = 0; i < initialCards.length; i++) {
   renderCard(initialCards[i].name, initialCards[i].link);
 }
@@ -48,14 +49,16 @@ function createCard(name, image) {
   return card;
 }
 
-function submitPopupAdd(evt) {
-  evt.preventDefault();
-
+function submitPopupAdd() {
   renderCard(popupAddInputName.value, popupAddInputSrc.value);
   closePopupAdd();
 }
 
 function openPopupAdd() {
+  popupAddInputName.value = '';
+  popupAddInputSrc.value = '';
+  disableButton(popupAdd);
+
   popupAdd.classList.add('popup_opened');
 }
 
@@ -67,16 +70,17 @@ function openPopupProfile() {
   popupProfileName.value = profileName.textContent;
   popupProfileStatus.value = profileStatus.textContent;
 
+
+
   popupProfile.classList.add('popup_opened');
+  enableButton(popupProfile);
 }
 
 function closePopupProfile() {
   popupProfile.classList.remove('popup_opened');
 }
 
-function submitPopupProfile(evt) {
-  evt.preventDefault();
-
+function submitPopupProfile() {
   profileName.textContent = popupProfileName.value;
   profileStatus.textContent = popupProfileStatus.value;
   closePopupProfile();
@@ -88,6 +92,90 @@ function openPopupCard() {
 
 function closePopupCard() {
   popupCard.classList.remove('popup-card__opened');
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+  return !inputElement.validity.valid;
+});
+}
+
+function showInputError(inputElement, errorMessage) {
+  const errorElement = document.querySelector(`.${inputElement.id}-error`);
+
+  inputElement.classList.add('popup__input_type_error');
+
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error-active');
+}
+
+function hideInputError(inputElement) {
+  const errorElement = document.querySelector(`.${inputElement.id}-error`);
+
+  inputElement.classList.remove('popup__input_type_error');
+
+  errorElement.classList.remove('popup__input-error-active');
+
+  errorElement.textContent = '';
+}
+
+function isValid(inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(inputElement, inputElement.validationMessage);
+  }
+  else {
+    hideInputError(inputElement);
+  }
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__submit-button_inactive');
+    buttonElement.setAttribute("disabled", "disabled");
+  }
+  else{
+    buttonElement.classList.remove('popup__submit-button_inactive');
+    buttonElement.removeAttribute("disabled", "disabled");
+  }
+}
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', function () {
+      isValid(inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+
+function enableButton(formElement) {
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+
+  buttonElement.classList.remove('popup__submit-button_inactive');
+  buttonElement.removeAttribute("disabled", "disabled");
+}
+
+function disableButton(formElement) {
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+
+  buttonElement.classList.add('popup__submit-button_inactive');
+  buttonElement.setAttribute("disabled", "disabled");
 }
 
 popupProfileSubmitButton.addEventListener('submit', submitPopupProfile);
